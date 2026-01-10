89d6c19154c8810712810cc01dc20e6e166dabe0
orudskyi <alex.rudskii@gmail.com>
2026-01-10 14:45:16 +0000

    refactor: made a littile changes

--- hashmap/group_anagrams.py
+++ hashmap/group_anagrams.py
@@ -5,28 +5,33 @@ Link: https://leetcode.com/problems/group-anagrams/description/?envType=study-pl
 Difficulty: Medium
 """
 
-from typing import List
 from collections import defaultdict
 
-class Solution:  
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         anagram_map = defaultdict(list)
-        
+
         for word in strs:
             sorted_key = "".join(sorted(word))
-            
+
             anagram_map[sorted_key].append(word)
-            
-        return list(anagram_map.values())  
-    
+
+        return list(anagram_map.values())
+
+
 def test_plus_one():
     s = Solution()
-    assert s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]) == [["bat"],["nat","tan"],["ate","eat","tea"]]
-    
-    
+    assert s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) == [
+        ["bat"],
+        ["nat", "tan"],
+        ["ate", "eat", "tea"],
+    ]
+
+
 if __name__ == "__main__":
     solver = Solution()
-    
-    result = solver.groupAnagrams(["eat","tea","tan","ate","nat","bat"])
+
+    result = solver.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
 
     print(f"Result:    {result}")
