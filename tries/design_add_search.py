911ba2bf864827e5dd143a532cb239852d8c18fc
orudskyi <alex.rudskii@gmail.com>
2026-03-12 18:40:58 +0000

    feat(tries): solve Design Add and Search Words Data Structure problem

---
+++ tries/design_add_search.py
@@ -0,0 +1,55 @@
+"""
+Problem 211. Design Add and Search Words Data Structure
+Category: Tries
+Link: https://neetcode.io/problems/design-word-search-data-structure/question
+Difficulty: Medium
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()        
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):   
+            cur = root
+            
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        return dfs(0, self.root)
+        
+        
+
+if __name__ == "__main__":
+    wordDictionary = WordDictionary()
+    wordDictionary.addWord("day")
+    wordDictionary.addWord("bay")
+    wordDictionary.addWord("may")
+    print(wordDictionary.search("say")) # return false
+    print(wordDictionary.search("day")) # return true
+    print(wordDictionary.search(".ay")) # return true
+    print(wordDictionary.search("b..")) # return true
